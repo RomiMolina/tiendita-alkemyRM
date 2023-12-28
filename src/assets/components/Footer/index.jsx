c7121833ea628800e591aebf3a4f2c4a583562a0
RomiMolina <roomi.molina@gmail.com>
2023-12-28 18:42:44 -0300

    footer

---
+++ src/assets/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import './footer.css';
+
+const Footer = ()=>{
+    return (
+        <footer className="footer">
+            <p>Número de contacto: 123-456-789</p>
+            <p>Horario de atención: Lunes a Viernes, 9:00 AM - 6:00 PM</p>
+            <p>Dirección: Calle Principal #123, Ciudad</p>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import NavBar from './assets/components/Nav/index.jsx'
 import './styles.css'
 import store from './redux/store/index.jsx'
 import { Provider } from 'react-redux'
+import Footer from './assets/components/Footer/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
   <React.StrictMode>
     <NavBar/>
     <App />
+    <Footer />
   </React.StrictMode>
 </Provider>
 )
